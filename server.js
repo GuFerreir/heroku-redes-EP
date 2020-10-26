c44ed957c3598fbd71cb7a5001c1a8eb64e9a5fa
GuFerreir <gu-ferreira@usp.br>
2020-10-26 12:25:49 -0300

    Projeto final comentado

--- server.js
+++ server.js
@@ -1,32 +1,34 @@
 
+//no começo setamos as dependencias
 const express = require('express');
 const path = require('path');
-
+//criação do servidor
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
-
+//a porta que irá escutar (no Heroku eles usam process.env.PORT como uma variável para saber a porta)
 var porta = process.env.PORT || 3000;
-
+//setamos a infra para renderizar o front
 app.use(express.static(path.join(__dirname)));
 app.set('views', path.join(__dirname));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
-
+//escutamos todas as requisições ao endereço seguido por '/' e renderezimos o index.html
 app.use('/', (req,res) => {
     res.render('index.html');
 });
 
-let messages = []; 
-
+//ligamos o socket
 io.on('connection', socket => {
     console.log(`Socket conectado: ${socket.id}`);
+    //enviamos via socket para o front o id (la eles escutarão e tratarão esse envio)
     socket.emit('idSocket', socket.id);
-
+    //escutamos o evendo sendMessage do front para receber o autor e a imagem
     socket.on('sendMessage', data => {
+        //enviamos à todos os dispositivos conectados os dados recebidos
         socket.broadcast.emit('receivedMessage', data);
     })
 });
-
+//servidor fica conectado na porta definida
 server.listen(porta);
 
